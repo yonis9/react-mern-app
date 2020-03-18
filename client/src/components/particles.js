c36294af7f3c75b4e0c09d81f49f0dcc977d62b5
Yoni Sisso <yonis9@walla.com>
2020-03-18 19:55:41 +0200

    changed background and particles colors

--- client/src/components/particles.js
+++ client/src/components/particles.js
@@ -4,15 +4,21 @@ import Particles from 'react-particles-js';
 class ParticleComponent extends React.Component{
     render(){
         return(
-            <div style={{backgroundColor:"#173f5f", position:"absolute", top:0, left:0, width:"100%", height:"100%"}}>
+            <div style={{backgroundColor:"#ffffff", position:"absolute", top:0, left:0, width:"100%", height:"100%"}}>
             <Particles
                 params={{
                     "particles": {
+                        "color": {
+                            "value": "ed8be8"
+                        },
                         "number": {
                             "value": 50
                         },
                         "size": {
                             "value": 3
+                        },
+                        "line_linked": {
+                            "color": '44b5ea'
                         }
                     },
                     "interactivity": {
